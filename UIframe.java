7b628006ea4ae0913d830fd0e8c071ffcefd1dd8
YaraChahine <79076389+YaraChahine@users.noreply.github.com>
2021-11-21 18:25:15 +0200

    Update UIframe.java

--- UIframe.java
+++ UIframe.java
@@ -24,6 +24,7 @@ import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.MenuElement;
 import javax.swing.MenuSelectionManager;
@@ -144,7 +145,8 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 		}
 		
 	}
-	}
+	
+ }
  
  
  class SignUpFrame extends JFrame implements ActionListener{
@@ -426,11 +428,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 			
 			}  
 			
-	  
-		
-			
-			
-	  
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -581,8 +578,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 			this.dispose();
 		}
 		else if (e.getSource()==ViewTrustedList) {
-			new ViewTrustedListFrame();
-			this.dispose();
+			new  ViewTrustedListFrame();
 		}
 		else if (e.getSource()==CheckFriends) {
 			new CheckFriendsFrame();
@@ -670,10 +666,147 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 }
  
  class ViewTrustedListFrame  extends JFrame implements ActionListener{
- 	 
-	 @Override
+ 	 public JPanel panel;
+ 	 public JLabel imgLabel;
+	 public JComboBox friendsMenu;
+	 public JButton addFriend;
+	 public String[] usersList= {"Hasan Mchawrab","Nahla Baalbaki","Ali Knayber","Yara Chahine"};
+	 public String[] columns= {"Name","Health Status"};
+	 public String[][] data= {{"Nahla Baalbaki","Safe"},{"Hassan Mchawrab","Contagious"}};
+	 
+	 ViewTrustedListFrame(){
+			
+		 Border border= BorderFactory.createRaisedSoftBevelBorder();
+
+
+			
+
+
+			this.setLayout(null);
+			this.setTitle("CovidLess");
+			this.setSize(385,640);
+			this.setLocation(300,15);
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+			this.setResizable(false);
+			this.setBackground(new Color(255, 255, 255));
+		
+			
+			panel= new JPanel();
+			panel.setBounds(0,0,385,640);
+			
+			panel.setBackground(new Color(255,255,255));
+			panel.setLayout(null);
+			 panel.setPreferredSize(new Dimension(375, 770));
+				this.add(panel);
+
+			    JScrollPane pane = new JScrollPane(panel);
+						this.setContentPane(pane);
+			
+		
+		
+
+					
+			JPanel headerShadow= new JPanel();
+			headerShadow.setBounds(0,0,375,48);
+			headerShadow.setBackground(new Color(220,220,220));
+			headerShadow.setOpaque(true);
+			headerShadow.setLayout(null);
+			panel.add(headerShadow);
+			
+			JPanel header= new JPanel();
+			header.setBounds(0,0,375,45);
+			header.setBackground(new Color(109,151,233));
+			header.setLayout(null);
+			headerShadow.add(header);
+			
+			
+			JLabel logo= new JLabel("CovidLess.");
+			logo.setBounds(10, 2,180, 40);
+			logo.setForeground(new Color(255,255,255));
+			logo.setFont(new Font("Bodoni Mt", Font.ITALIC, 32));
+			header.add(logo);
+			
+			
+			JPanel titleShadow= new JPanel();
+			titleShadow.setBounds(87,85,180,48);
+			titleShadow.setBackground(new Color(210,210,210));
+			titleShadow.setOpaque(true);
+			titleShadow.setLayout(null);
+			panel.add(titleShadow); 
+			
+			JPanel TitleBackground= new JPanel();
+			TitleBackground.setBounds(2,0,176,46);
+			TitleBackground.setBackground(new Color(255,255,255));
+			TitleBackground.setOpaque(true);
+			TitleBackground.setLayout(null);
+			TitleBackground.setBorder(null);
+			titleShadow.add(TitleBackground);
+			
+			JLabel trustedLabel= new JLabel("Trusted Friends");
+			trustedLabel.setFont(new Font("Arial", Font.BOLD,20));
+			trustedLabel.setForeground(new Color(109,151,233));
+			trustedLabel.setBounds(13,0,170,40);
+			TitleBackground.add(trustedLabel);
+			
+			JLabel label1= new JLabel("Your trusted friends can see your health status.");
+			label1.setFont(new Font("Arial", Font.BOLD, 14));
+			label1.setForeground(new Color(120,120,120));
+			label1.setBounds(10,160,350,40);
+			panel.add(label1);
+			
+			
+			JLabel label2= new JLabel("Click on \"add\" to add a trusted friend");
+			label2.setFont(new Font("Arial", Font.BOLD, 13));
+			label2.setForeground(new Color(120,120,120));
+			label2.setBounds(65,180,260,40);
+			panel.add(label2);
+			
+			
+			JLabel usersLabel= new JLabel("Users: ");
+			usersLabel.setFont(new Font("Arial", Font.BOLD, 12));
+			usersLabel.setForeground(new Color(109,151,233));
+			usersLabel.setBounds(10,230,90,30);
+			panel.add(usersLabel);
+			
+
+			friendsMenu= new JComboBox(usersList);
+			friendsMenu.setBounds(60,230,190,30);
+			friendsMenu.setBackground(new Color(255,255,255));
+			panel.add(friendsMenu);
+			
+			
+			addFriend= new JButton("Add");
+			addFriend.setBackground(new Color(109,151,233));
+			addFriend.setBounds(265,230,70,30);
+			addFriend.setForeground(new Color(255,255,255));
+			panel.add(addFriend);
+			
+			
+			JTable table=new JTable(data,columns);
+			table.setBounds(20,340,300,200);
+			panel.add(table);
+			/*
+			JPanel header= new JPanel();
+			header.setBounds(0,0,375,45);
+			header.setBackground(new Color(109,151,233));
+			header.setLayout(null);
+			headerShadow.add(header);
+			
+			
+			JLabel logo= new JLabel("CovidLess.");
+			logo.setBounds(10, 2,180, 40);
+			logo.setForeground(new Color(255,255,255));
+			logo.setFont(new Font("Bodoni Mt", Font.ITALIC, 32));
+			header.add(logo);	
+			*/
+			
+			
+			this.setVisible(true);
+		 
+	 }
 	 public void actionPerformed(ActionEvent e) {
 	 	// TODO Auto-generated method stub
+		
 	 	
 	 }
  }
